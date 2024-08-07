b63c1b8a0362704b7abc8016a5ac973d75555446
Iterasys <correia@iterasys.com.br>
2024-08-06 21:36:32 -0300

    first commit

---
+++ test/api/pet.spec.js
@@ -0,0 +1,25 @@
+// Bibliotecas e Framework
+const supertest = require('supertest')
+
+const petId = 173218101
+
+// Em JavaScript, Classe é opcional, mas pode agrupar em uma Describe
+describe('API PetStore Swagger - Entidade Pet', () =>{
+
+    // Atributos do grupo/describe
+    const request = supertest('https://petstore.swagger.io/v2') // BaseURL
+
+    // Funções ou Métodos: Its
+    it('POST Pet', () => {
+        // Atributos, Campos, Características, Configurações
+        
+        // Funções de Apoio (Opcional)
+
+        // Funções de Teste em Si
+
+
+    }) // Final do método POST
+
+ 
+}) // termina a describe
+
