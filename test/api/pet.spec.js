49612387c90c25e817b0152af6ea4ac90f9de699
Iterasys <correia@iterasys.com.br>
2024-08-06 21:54:57 -0300

    Escrevi um comentario Oi

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -8,18 +8,17 @@ describe('API PetStore Swagger - Entidade Pet', () =>{
 
     // Atributos do grupo/describe
     const request = supertest('https://petstore.swagger.io/v2') // BaseURL
-
+ 
     // Funções ou Métodos: Its
     it('POST Pet', () => {
         // Atributos, Campos, Características, Configurações
-        
+       
         // Funções de Apoio (Opcional)
 
         // Funções de Teste em Si
 
-
     }) // Final do método POST
 
- 
+// Oi
 }) // termina a describe
 
